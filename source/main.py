5a12e3f883b184c688d133e90200d4067d21ecb5
r0w4n <6553343+r0w4n@users.noreply.github.com>
2023-02-02 20:05:45 +0000

    changed the recycle time

--- source/main.py
+++ source/main.py
@@ -22,9 +22,8 @@ def main():
                 "youtube", "v3", credentials=youtube_auth.handle_auth(camera["name"])
             )
 
-            # Check to see if it midnight and if it is kill the current schedule
-            now = datetime.datetime.now()
-            if now.hour == 0 and now.minute == 0 and youtube_schedule.has_scheduled_broadcast(youtube):
+            # Check to see if it time to recycle and if it is kill the current schedule
+            if is_recycle_time() and youtube_schedule.has_scheduled_broadcast(youtube):
                 manage_ending_broadcast(camera, youtube)
 
             # Checks to see whether there are any scheduled broadcasts and if there isn't creates a schedule
@@ -34,7 +33,7 @@ def main():
             # checks to see whether there are any unhealthy streams and if there are kill them
             if not youtube_streamer.is_live_stream_healthy(youtube):
                 manage_unhealthy_stream(camera)
-            
+
             # checks to see whether a scheduled broadcast is inactive
             if youtube_schedule.has_inactive_broadcast(youtube):
                 manage_inactive_broadcast(camera)
@@ -42,14 +41,24 @@ def main():
     except HttpError as err:
         print(err)
 
+
+def is_recycle_time():
+    now = datetime.datetime.now()
+
+    if (now.hour == 8 or now.hour == 20 or now.hour == 2) and now.minute == 0:
+        return True
+    return False
+
+
 def manage_ending_broadcast(camera, youtube):
-    print(f"""It's midnight and {camera["name"]} has scheduled stream on youtube""")
+    print(f"""It's recycle time and {camera["name"]} has scheduled stream on youtube""")
     print(f"""Ending {camera["name"]} scheduled stream on youtube""")
     youtube_schedule.end_schedule(youtube)
     if youtube_streamer.is_streaming(camera):
         print(f"""killing screen session for {camera["name"]}""")
         youtube_streamer.kill_stream(camera)
         time.sleep(5)
+
 
 def manage_schedule(camera, youtube):
     print(f"""{camera["name"]} has no scheduled stream on youtube""")
@@ -58,6 +67,7 @@ def manage_schedule(camera, youtube):
         youtube_streamer.kill_stream(camera)
     print(f"""creating schedule for {camera["name"]} on youtube""")
     youtube_schedule.do_schedule(youtube, camera)
+
 
 def manage_unhealthy_stream(camera):
     print(f"""{camera["name"]} stream on youtube is unhealthy""")
@@ -68,6 +78,7 @@ def manage_unhealthy_stream(camera):
     print(f"""starting stream for {camera["name"]}""")
     youtube_streamer.start_stream(camera)
 
+
 def manage_inactive_broadcast(camera):
     print(f"""{camera["name"]} has a scheduled broadcast that hasn't been started""")
     if youtube_streamer.is_streaming(camera):
@@ -76,6 +87,7 @@ def manage_inactive_broadcast(camera):
         time.sleep(5)
     print(f"""starting stream for {camera["name"]}""")
     youtube_streamer.start_stream(camera)
+
 
 if __name__ == "__main__":
     main()
