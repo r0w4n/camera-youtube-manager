b46cfef4c308574cfed5594e291ed19a6624e7f8
r0w4n <6553343+r0w4n@users.noreply.github.com>
2026-04-21 15:55:11 +0100

    improved readability and testing

--- source/settings.py
+++ source/settings.py
@@ -4,7 +4,10 @@ from pathlib import Path
 from camera_config import AppSettings
 
 
+def get_settings_file_path():
+    return Path(__file__).resolve().parent.parent / "settings.json"
+
+
 def get_settings():
-    settings_file_path = Path(__file__).resolve().parent.parent / "settings.json"
-    with settings_file_path.open() as settings_file:
+    with get_settings_file_path().open() as settings_file:
         return AppSettings.from_dict(json.load(settings_file))
